033e163259a21fbb9c16f75c1aeda58a7f01a202
mostafa <mostafametwaly86@gmail.com>
2022-04-11 11:55:43 +0200

    gradle commit

--- src/test/java/login/Login_Test.java
+++ src/test/java/login/Login_Test.java
@@ -30,13 +30,13 @@ public class Login_Test {
     public void setupMethod() throws InterruptedException{
 
         //Configuration.baseUrl="https://";
-        Configuration.browserSize = "1296x687";
-        Configuration.holdBrowserOpen= true;
-        Configuration.browser="edge";
+        //Configuration.browserSize = "1296x687";
+        //Configuration.holdBrowserOpen= true;
+        //Configuration.browser="edge";
         //com.codeborne.selenide.Configuration.chromeSwitches = "no-sandbox";
         //Configuration.headless= true;
 
-        Configuration.browserPosition="0x0";
+        //Configuration.browserPosition="0x0";
 
         //open("www.google.com");
         //open("https://the-internet.herokuapp.com/");
@@ -52,62 +52,62 @@ public class Login_Test {
         WebDriverManager.chromedriver().setup();
         WebDriver driver= new ChromeDriver();
         driver.navigate().to("https://the-internet.herokuapp.com/nested_frames");
-        System.out.println($$(By.tagName("frame")).size());
-        switchTo().frame(0);
-        System.out.println($$(By.tagName("frame")).size());
-        for (int i=0;i<$$(By.tagName("frame")).size();i++){
-            switchTo().frame(i);
-            System.out.println($(By.tagName("body")).getText());
-            switchTo().parentFrame();
-        }
-        switchTo().parentFrame();
-        switchTo().frame(1);
-        System.out.println($(By.tagName("body")).getText());
+        //System.out.println($$(By.tagName("frame")).size());
+        driver.switchTo().frame(0);
+       // System.out.println($$(By.tagName("frame")).size());
+//        for (int i=0;i<$$(By.tagName("frame")).size();i++){
+//            driver.switchTo().frame(i);
+//            System.out.println($(By.tagName("body")).getText());
+//            driver.switchTo().parentFrame();
+//        }
+        driver.switchTo().parentFrame();
+        driver.switchTo().frame(1);
+        //System.out.println($(By.tagName("body")).getText());
        Assert.assertTrue(true);
     }
-    @Test
-    public void switchToAlert() {
-        open("https://the-internet.herokuapp.com/javascript_alerts");
-        $(By.tagName("button")).click();
-        Alert alert = switchTo().alert();
-        String actual= alert.getText();
-        alert.accept();
-        String expected= "I am a JS Alert";
-        //Assert.
-        assertEquals(actual, expected);
-    }
-    @Test
-    public void findelements() {
-        open("https://the-internet.herokuapp.com/");
-        ElementsCollection coll= $$(By.tagName("a"));
-        List<String> x= coll.texts();
-        int size = coll.size();
-        System.out.println(size);
-        x.forEach(e -> System.out.println(e));
-        coll.filter(Condition.ownText("File"));
-        for  (SelenideElement element: coll
-        ) {
-            System.out.println(element.getText());
-        }
-    }
-
-    @Test
-    public void selectHandling(){
-
-        open("https://the-internet.herokuapp.com/dropdown");
-        $(By.id("dropdown")).selectOption(2);
-    }
-    @Test
-    public void mouseAction() throws InterruptedException {
-        open("https://jqueryui.com/droppable/");
-        switchTo().frame(0);
-        //actions().moveToElement($(By.id("column-a"))).doubleClick().build().perform();
-        Thread.sleep(7000);
-        actions().dragAndDrop($(By.id("draggablee")), $(By.id("droppable"))).build().perform();
-        Thread.sleep(3000);
-    }
-    @Test
-    public void helloWorld(){
-        System.out.println("Hello World This is mostafa from the Earth");
-    }
+//    @Test
+//    public void switchToAlert() {
+//        open("https://the-internet.herokuapp.com/javascript_alerts");
+//        $(By.tagName("button")).click();
+//        Alert alert = switchTo().alert();
+//        String actual= alert.getText();
+//        alert.accept();
+//        String expected= "I am a JS Alert";
+//        //Assert.
+//        assertEquals(actual, expected);
+//    }
+//    @Test
+//    public void findelements() {
+//        open("https://the-internet.herokuapp.com/");
+//        ElementsCollection coll= $$(By.tagName("a"));
+//        List<String> x= coll.texts();
+//        int size = coll.size();
+//        System.out.println(size);
+//        x.forEach(e -> System.out.println(e));
+//        coll.filter(Condition.ownText("File"));
+//        for  (SelenideElement element: coll
+//        ) {
+//            System.out.println(element.getText());
+//        }
+//    }
+//
+//    @Test
+//    public void selectHandling(){
+//
+//        open("https://the-internet.herokuapp.com/dropdown");
+//        $(By.id("dropdown")).selectOption(2);
+//    }
+//    @Test
+//    public void mouseAction() throws InterruptedException {
+//        open("https://jqueryui.com/droppable/");
+//        switchTo().frame(0);
+//        //actions().moveToElement($(By.id("column-a"))).doubleClick().build().perform();
+//        Thread.sleep(7000);
+//        actions().dragAndDrop($(By.id("draggablee")), $(By.id("droppable"))).build().perform();
+//        Thread.sleep(3000);
+//    }
+//    @Test
+//    public void helloWorld(){
+//        System.out.println("Hello World This is mostafa from the Earth");
+//    }
 }
